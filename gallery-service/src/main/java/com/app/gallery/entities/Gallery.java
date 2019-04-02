3ac1f8428dec0ec1884f93fc62200af39c3b1228
krishnan muthukumar <krish@cardinality.ai>
2019-04-02 22:39:02 +0530

    added missing codes

---
+++ gallery-service/src/main/java/com/app/gallery/entities/Gallery.java
@@ -0,0 +1,34 @@
+package com.app.gallery.entities;
+
+import java.util.List;
+
+public class Gallery {
+	
+	private Integer id;
+	private List<Object> images;
+	
+	public Gallery(int galleryId) {
+
+	}
+	
+	public Gallery() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public List<Object> getImages() {
+		return images;
+	}
+
+	public void setImages(List<Object> images) {
+		this.images = images;
+	}
+
+}
